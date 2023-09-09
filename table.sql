1a33d4393eb5780f666c3ef478a52e0a9869e983
s24808 <93827195+s24808@users.noreply.github.com>
2023-09-09 18:36:00 +0200

    Create table.sql

---
+++ table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE currency_query1 (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    currency TINYINT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    calculated_rate DOUBLE NOT NULL,
+    query_date DATE NOT NULL,
+    query_time TIME NOT NULL,
+    result_count INT NOT NULL
+);
